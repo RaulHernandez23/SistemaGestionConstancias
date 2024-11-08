5bd7dc9071b9fc729a575903498106c052881934
MIGUEL ANGEL MORALES CRUZ <zS21026433@estudiantes.uv.mx>
2024-11-08 12:47:09 -0600

    Se completo el frontEnd del botón cerrar sesión

--- src/main/java/org/lossuperconocidos/sistemagestionconstancias/controladores/FXMLMenuDocente.java
+++ src/main/java/org/lossuperconocidos/sistemagestionconstancias/controladores/FXMLMenuDocente.java
@@ -1,5 +1,35 @@
 package org.lossuperconocidos.sistemagestionconstancias.controladores;
 
-public class FXMLMenuDocente {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.awt.event.MouseEvent;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class FXMLMenuDocente implements Initializable {
+
+    @FXML
+    private ImageView ivSalir;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void hoverInSalir(javafx.scene.input.MouseEvent mouseEvent) {
+        ivSalir.setImage(new Image(
+                getClass().getResource("/imagenes/logoSalir2.png").toExternalForm()
+        ));
+    }
+
+    public void hoverOutSalir(javafx.scene.input.MouseEvent mouseEvent) {
+        ivSalir.setImage(new Image(
+                getClass().getResource("/imagenes/logoSalir.png").toExternalForm()
+        ));
+    }
 }
