fd4a9b8da792d7c224b95a8fe7e0918c5c481656
Raul <raulh230600@gmail.com>
2024-11-16 10:00:44 -0600

    feat(ParticipacionProyecto): añadida funcionalidad a boton ProyectoCampo

--- src/main/java/org/lossuperconocidos/sistemagestionconstancias/controladores/FXMLRegistrarParticipacion.java
+++ src/main/java/org/lossuperconocidos/sistemagestionconstancias/controladores/FXMLRegistrarParticipacion.java
@@ -1,6 +1,12 @@
 package org.lossuperconocidos.sistemagestionconstancias.controladores;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FXMLRegistrarParticipacion
 {
@@ -14,6 +20,17 @@ public class FXMLRegistrarParticipacion
 
     @javafx.fxml.FXML
     public void actionProyecto(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/lossuperconocidos/sistemagestionconstancias/FXMLParticipacionProyecto.fxml"));
+            Scene scene = new Scene(loader.load());
+
+            Stage escenario = new Stage();
+            escenario.setScene(scene);
+            escenario.setTitle("Registrar participacion:  Proyecto de campo");
+            escenario.show();
+        } catch (IOException ioEx){
+            ioEx.printStackTrace();
+        }
     }
 
     @javafx.fxml.FXML
