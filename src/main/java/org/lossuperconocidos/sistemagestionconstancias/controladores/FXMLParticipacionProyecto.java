adf846eb2bb7a81baca93aa1a8cbe6c0faf45ad6
Raul <raulh230600@gmail.com>
2024-11-15 20:01:23 -0600

    feat(ParticipacionProyecto): creada vista y controlador de la participacion del proyecto

---
+++ src/main/java/org/lossuperconocidos/sistemagestionconstancias/controladores/FXMLParticipacionProyecto.java
@@ -0,0 +1,53 @@
+package org.lossuperconocidos.sistemagestionconstancias.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class FXMLParticipacionProyecto {
+    @javafx.fxml.FXML
+    private TextField txtAlumno;
+    @javafx.fxml.FXML
+    private TextField txtProyecto;
+    @javafx.fxml.FXML
+    private Label lblErrorProyecto;
+    @javafx.fxml.FXML
+    private Label lblErrorImpacto;
+    @javafx.fxml.FXML
+    private Label lblErrorLugar;
+    @javafx.fxml.FXML
+    private ComboBox cbDocentes;
+    @javafx.fxml.FXML
+    private Label lblErrorAlumno;
+    @javafx.fxml.FXML
+    private ComboBox cbPeriodos;
+    @javafx.fxml.FXML
+    private TextField txtLugar;
+    @javafx.fxml.FXML
+    private Button btnEliminar;
+    @javafx.fxml.FXML
+    private TextField txtImpacto;
+    @javafx.fxml.FXML
+    private TableColumn tcAlumnos;
+    @javafx.fxml.FXML
+    private TableView tvAlumnos;
+    @javafx.fxml.FXML
+    private Button btnAñadir;
+    @javafx.fxml.FXML
+    private Button btnRegistrar;
+
+    @javafx.fxml.FXML
+    public void actionCancelar(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void actionAñadir(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void actionEliminar(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void actionRegistrar(ActionEvent actionEvent) {
+    }
+}
