98d6842f4569309e4b7a4db9e26f9b80cc8ec95f
Albhieri Cristoff Villa Contreras <alcrivico@gmail.com>
2024-11-30 07:55:57 -0600

    Se estandarizaron los scripts de bases de datos

--- bd/crear_tablas.sql
+++ bd/crear_tablas.sql
@@ -1,57 +1,27 @@
 CREATE DATABASE IF NOT EXISTS sistema_constancias;
 USE sistema_constancias;
 
-CREATE TABLE `CATEGORIA`(
+CREATE TABLE `categoria`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `nombre` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `PROGRAMA_EDUCATIVO`(
+
+CREATE TABLE `programa_educativo`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `nombre` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `IMPARTICION_EE`(
-    `participacion_id` INT NOT NULL,
-    `experiencia_educativa` VARCHAR(255) NOT NULL,
-    `bloque` VARCHAR(255) NOT NULL,
-    `creditos` INT NOT NULL,
-    `horas` INT NOT NULL,
-    `meses` INT NOT NULL,
-    `seccion` INT NOT NULL,
-    `semanas` INT NOT NULL,
-    `programa_educativo_id` INT NOT NULL
-);
-CREATE TABLE `PLADEA`(
-    `participacion_id` INT NOT NULL,
-    `acciones` VARCHAR(255) NOT NULL,
-    `eje_estrategico` VARCHAR(255) NOT NULL,
-    `metas` VARCHAR(255) NOT NULL,
-    `objetivos_generales` VARCHAR(255) NOT NULL,
-    `programa_estrategico` VARCHAR(255) NOT NULL
-);
-CREATE TABLE `PROYECTO_CAMPO`(
-    `participacion_id` INT NOT NULL,
-    `proyecto_realizado` VARCHAR(255) NOT NULL,
-    `impacto_obtenido` VARCHAR(255) NOT NULL,
-    `lugar` VARCHAR(255) NOT NULL,
-    `nombre_alumnos` VARCHAR(255) NOT NULL
-);
-CREATE TABLE `JURADO`(
-    `participacion_id` INT NOT NULL,
-    `titulo_trabajo` VARCHAR(255) NOT NULL,
-    `fecha_presentacion` DATE NOT NULL,
-    `modalidad` VARCHAR(255) NOT NULL,
-    `nombre_alumnos` VARCHAR(255) NOT NULL,
-    `resultado_obtenido` VARCHAR(255) NOT NULL
-);
-CREATE TABLE `TIPO_CONTRATACION`(
+
+CREATE TABLE `tipo_contratacion`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `nombre` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `TIPO_USUARIO`(
+
+CREATE TABLE `tipo_usuario`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `nombre` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `USUARIO`(
+
+CREATE TABLE `usuario`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `no_personal` VARCHAR(255) NOT NULL,
     `nombre` VARCHAR(255) NOT NULL,
@@ -61,44 +31,74 @@ CREATE TABLE `USUARIO`(
     `password` VARCHAR(255) NOT NULL,
     `firma_digital` VARCHAR(255) NULL,
     `categoria_id` INT NULL,
-    `tipo_contratacion_id` INT NOT NULL
+    `tipo_contratacion_id` INT NOT NULL,
+    FOREIGN KEY (`categoria_id`) REFERENCES `categoria`(`id`),
+    FOREIGN KEY (`tipo_contratacion_id`) REFERENCES `tipo_contratacion`(`id`)
 );
-CREATE TABLE `USUARIO_TIPO_USUARIO`(
+
+CREATE TABLE `usuario_tipo_usuario`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `tipo_usuario_id` INT NOT NULL,
-    `usuario_id` INT NOT NULL
+    `usuario_id` INT NOT NULL,
+    FOREIGN KEY (`tipo_usuario_id`) REFERENCES `tipo_usuario`(`id`),
+    FOREIGN KEY (`usuario_id`) REFERENCES `usuario`(`id`)
 );
-CREATE TABLE `PERIODO_ESCOLAR`(
+
+CREATE TABLE `periodo_escolar`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `fecha_inicio` DATE NOT NULL,
     `fecha_fin` DATE NOT NULL,
     `nombre` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `PARTICIPACION`(
+
+CREATE TABLE `participacion`(
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `tipo_participacion` VARCHAR(255) NOT NULL,
     `docente_id` INT NOT NULL,
-    `periodo_escolar_id` INT NOT NULL
+    `periodo_escolar_id` INT NOT NULL,
+    FOREIGN KEY (`docente_id`) REFERENCES `usuario`(`id`),
+    FOREIGN KEY (`periodo_escolar_id`) REFERENCES `periodo_escolar`(`id`)
 );
-ALTER TABLE
-    `PROYECTO_CAMPO` ADD CONSTRAINT `FK_proyecto_campo_participacion` FOREIGN KEY(`participacion_id`) REFERENCES `PARTICIPACION`(`id`);
-ALTER TABLE
-    `JURADO` ADD CONSTRAINT `FK_jurado_participacion` FOREIGN KEY(`participacion_id`) REFERENCES `PARTICIPACION`(`id`);
-ALTER TABLE
-    `USUARIO` ADD CONSTRAINT `FK_usuario_tipo_contratacion` FOREIGN KEY(`tipo_contratacion_id`) REFERENCES `TIPO_CONTRATACION`(`id`);
-ALTER TABLE
-    `PARTICIPACION` ADD CONSTRAINT `FK_participacion_docente` FOREIGN KEY(`docente_id`) REFERENCES `USUARIO`(`id`);
-ALTER TABLE
-    `USUARIO_TIPO_USUARIO` ADD CONSTRAINT `FK_usuario_tipo_usuario_tipo_usuario` FOREIGN KEY(`tipo_usuario_id`) REFERENCES `TIPO_USUARIO`(`id`);
-ALTER TABLE
-    `IMPARTICION_EE` ADD CONSTRAINT `FK_imparticion_ee_participacion` FOREIGN KEY(`participacion_id`) REFERENCES `PARTICIPACION`(`id`);
-ALTER TABLE
-    `PLADEA` ADD CONSTRAINT `FK_pladea_participacion` FOREIGN KEY(`participacion_id`) REFERENCES `PARTICIPACION`(`id`);
-ALTER TABLE
-    `PARTICIPACION` ADD CONSTRAINT `FK_participacion_periodo_escolar` FOREIGN KEY(`periodo_escolar_id`) REFERENCES `PERIODO_ESCOLAR`(`id`);
-ALTER TABLE
-    `USUARIO_TIPO_USUARIO` ADD CONSTRAINT `FK_usuario_tipo_usuario_usuario` FOREIGN KEY(`usuario_id`) REFERENCES `USUARIO`(`id`);
-ALTER TABLE
-    `USUARIO` ADD CONSTRAINT `FK_usuario_categoria` FOREIGN KEY(`categoria_id`) REFERENCES `CATEGORIA`(`id`);
-ALTER TABLE
-    `IMPARTICION_EE` ADD CONSTRAINT `FK_imparticion_ee_programa_educativo` FOREIGN KEY(`programa_educativo_id`) REFERENCES `PROGRAMA_EDUCATIVO`(`id`);
+
+CREATE TABLE `imparticion_ee`(
+    `participacion_id` INT NOT NULL,
+    `experiencia_educativa` VARCHAR(255) NOT NULL,
+    `bloque` VARCHAR(255) NOT NULL,
+    `creditos` INT NOT NULL,
+    `horas` INT NOT NULL,
+    `meses` INT NOT NULL,
+    `seccion` INT NOT NULL,
+    `semanas` INT NOT NULL,
+    `programa_educativo_id` INT NOT NULL,
+    FOREIGN KEY (`participacion_id`) REFERENCES `participacion`(`id`),
+    FOREIGN KEY (`programa_educativo_id`) REFERENCES `programa_educativo`(`id`)
+);
+
+CREATE TABLE `pladea`(
+    `participacion_id` INT NOT NULL,
+    `acciones` VARCHAR(255) NOT NULL,
+    `eje_estrategico` VARCHAR(255) NOT NULL,
+    `metas` VARCHAR(255) NOT NULL,
+    `objetivos_generales` VARCHAR(255) NOT NULL,
+    `programa_estrategico` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`participacion_id`) REFERENCES `participacion`(`id`)
+);
+
+CREATE TABLE `proyecto_campo`(
+    `participacion_id` INT NOT NULL,
+    `proyecto_realizado` VARCHAR(255) NOT NULL,
+    `impacto_obtenido` VARCHAR(255) NOT NULL,
+    `lugar` VARCHAR(255) NOT NULL,
+    `nombre_alumnos` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`participacion_id`) REFERENCES `participacion`(`id`)
+);
+
+CREATE TABLE `jurado`(
+    `participacion_id` INT NOT NULL,
+    `titulo_trabajo` VARCHAR(255) NOT NULL,
+    `fecha_presentacion` DATE NOT NULL,
+    `modalidad` VARCHAR(255) NOT NULL,
+    `nombre_alumnos` VARCHAR(255) NOT NULL,
+    `resultado_obtenido` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`participacion_id`) REFERENCES `participacion`(`id`)
+);
